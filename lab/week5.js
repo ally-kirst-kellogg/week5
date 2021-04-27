4c38366f282085ca50478154619d2989b46fcc45
ally-kirst-kellogg <ally.kirst@kellogg.northwestern.edu>
2021-04-27 11:30:52 -0500

    lab

--- lab/week5.js
+++ lab/week5.js
@@ -15,26 +15,54 @@
 
 window.addEventListener('DOMContentLoaded', async function() {
   // Get a reference to the "get weather" button
-
+let weatherButton = document.querySelector(`button`)
   // When the "get weather" button is clicked:
+submitButton.addEventListener(`click`, async function(event) {
+// - Ignore the default behavior of the button
+event.preventDefault()
+ // - Get a reference to the element containing the user-entered location
+let locationElement = document.querySelector(`#location`)
+// - Get the user-entered location from the element's value
 
-    // - Ignore the default behavior of the button
+let location = locationElement.value
+// - Check to see if the user entered anything; if so:
+if (location.length >0) {
+  // - Construct a URL to call the WeatherAPI.com API
+  let url = `https://api.weatherapi.com/v1/forecast.json?key=e8be196480e347b3b62160258212704&q=${location}`
 
-    // - Get a reference to the element containing the user-entered location
+ // - Fetch the url, wait for a response, store the response in memory
+ let response = await fetch(url)
 
-    // - Get the user-entered location from the element's value
+// - Ask for the json-formatted data from the response, wait for the data, store it in memory
+ let json = await response.json()
+// - Write the json-formatted data to the JavaScript console
+ // - Write the json-formatted data to the JavaScript console
+ console.log(json)
 
-    // - Check to see if the user entered anything; if so:
+ // - Store the returned location, current weather conditions, the forecast as three separate variables
+ let interpretedLocation = json.location
+ let currentWeather = json.current
+ let dailyForecast = json.forecast
 
-      // - Construct a URL to call the WeatherAPI.com API
+ // Store a reference to the "current" element
+ let currentElement = document.querySelector(`.current`)
 
-      // - Fetch the url, wait for a response, store the response in memory
+   // Fill the current element with the location and current weather conditions
+   currentElement.innerHTML = `
+   <div class="text-center space-y-2">
+     <div class="font-bold text-3xl">Current Weather for ${interpretedLocation.name}, ${interpretedLocation.region}</div>
+     <div class="font-bold">
+       <img src="https://cdn.weatherapi.com/weather/64x64/day/116.png" class="inline-block">
+       <span class="temperature">60</span>° 
+       and
+       <span class="conditions">Partly Cloudy</span>
+     </div>
+   </div>
+ `
 
-      // - Ask for the json-formatted data from the response, wait for the data, store it in memory
 
-      // - Write the json-formatted data to the JavaScript console
+}
 
-      // - Store the interpreted location, current weather conditions, the forecast as three separate variables
+})
 
-      // - Continue the recipe yourself!
 })
